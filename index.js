68045d0fec2fef8da57f9dbc3f1a9ccbf852ba5a
jfrisina <jaki.frisina@gmail.com>
2024-06-05 21:54:25 -0700

    link user.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const userRoutes = require('./routes/user');
 
+app.use('/user,', userRoutes);
 
 app.listen(port, () => {
-    console.log(`Server listening on port: ${port}.`);
+  console.log(`Server listening on port: ${port}.`);
   });
 
+  // begin middleware
+  const logReq = function (req, res, next) {
+    console.log("Request Received");
+    next();
+  };
+  
+  app.use(logReq);
+  // end middleware
   
 app.get("/", (req, res) => {
     res.send("Hello Express!");
@@ -23,4 +33,6 @@ app.get("/", (req, res) => {
   app.post("/user", (req, res) => {
     res.send("Received a POST request for user!");
   });
-  
+
+  console.log("----------SUCCESS ---------");
+
